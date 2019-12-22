a7d1026fb3cbd6728b2fbb3e131b6a23b61d7c3c
artmadeit <this.arthur.email@gmail.com>
2019-12-22 16:34:18 -0500

    feat: banner only in homepage

--- src/App.js
+++ src/App.js
@@ -19,6 +19,16 @@ const Banner = () => (
       <Box direction="row" gap="small" align="center">
         <Heading level="3"> Conectate, Alaba, Sierve </Heading>
       </Box>
+      <Box width="small">
+        <Link to="articles">
+          <Button
+            reverse
+            label="Empieza ya!"
+            primary
+            icon={<LinkNext />}
+          ></Button>
+        </Link>
+      </Box>
     </Box>
   </Box>
 )
@@ -26,48 +36,5 @@ const Banner = () => (
 export default () => (
   <Layout>
     <Banner />
-    <Box>
-      <Box>
-        <Box
-          direction="row-responsive"
-          gap="small"
-          justify="between"
-          align="center"
-        >
-          <Box width="large" pad="medium">
-            <Paragraph size="large">Bienvenido a casa! Jesús te ama!</Paragraph>
-            <Paragraph>
-              La oportunidad que tanto esperabas, para escuchar la palabra de
-              nuestro querido amigo Jesús, escuchar alabanzas.
-            </Paragraph>
-
-            <Box width="small">
-              <Link to="articles">
-                <Button
-                  reverse
-                  label="Empieza ya!"
-                  primary
-                  icon={<LinkNext />}
-                ></Button>
-              </Link>
-            </Box>
-          </Box>
-          <Box height="medium" width="large">
-            <Image
-              fit="contain"
-              src="https://scontent.flim1-2.fna.fbcdn.net/v/t1.0-9/79137769_3158429467505064_8394860591914680320_n.jpg?_nc_cat=102&_nc_ohc=jP_Qd-sxd6QAQlRw7l6f9qH6ZyTLN037IM-OvkB50MCyAQ1zj0Ao2RFdQ&_nc_ht=scontent.flim1-2.fna&oh=75ed56c9f143fbef3502e7d6638833b5&oe=5EA7279C"
-              alt="Servicios"
-            ></Image>
-          </Box>
-          <Box height="medium" width="large">
-            <Image
-              fit="contain"
-              src="http://caminodevida.com/images/bauti.jpg?crc=466340632"
-              alt="Grupos"
-            ></Image>
-          </Box>
-        </Box>
-      </Box>
-    </Box>
   </Layout>
 )
