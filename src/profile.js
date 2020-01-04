11e08ec6f062488cf511da6c03a71c53d30a2013
artmadeit <this.arthur.email@gmail.com>
2020-01-04 14:50:28 -0500

    feat: add schedule and address

--- src/profile.js
+++ src/profile.js
@@ -1,5 +1,7 @@
 import React from "react"
 import GoogleMapReact from "google-map-react"
+import { Layout } from "./layout"
+import { Box, Grid } from "grommet"
 
 const parties = [
   {
@@ -16,7 +18,7 @@ const parties = [
 const AnyReactComponent = ({ text }) => <div>{text}</div>
 
 const Map = ({ latitude, longitude, zoom = 16, text }) => (
-  <div style={{ height: "400px", width: "400px" }}>
+  <Box width="xlarge" height="medium">
     <GoogleMapReact
       bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_KEY }}
       defaultCenter={{ lat: latitude, lng: longitude }}
@@ -24,20 +26,82 @@ const Map = ({ latitude, longitude, zoom = 16, text }) => (
     >
       <AnyReactComponent lat={latitude} lng={longitude} text={text} />
     </GoogleMapReact>
-  </div>
+  </Box>
 )
+
+const mainSchedule = ["Jueves 8:00pm", "Domingo 9:00am y 11:00am"]
+
+const scheduleByGroup = [
+  {
+    name: "Mujeres",
+    schedule: ["Miércoles 9:00am"]
+  },
+  {
+    name: "Jóvenes (UNO)",
+    schedule: ["Viernes 8:00pm"]
+  },
+  {
+    name: "Jóvenes Adultos (JVP)",
+    schedule: ["Miércoles 7:30pm"]
+  },
+  {
+    name: "Intercesión",
+    schedule: ["Lunes 9:00am"]
+  }
+]
 
 export const Profile = ({ partyId }) => {
   const party = parties.find(x => x.id === partyId)
 
   return (
-    <div>
-      <h3>{party.name}</h3>
+    <Layout>
+      <h1>{party.name}</h1>
+      <ScheduleAndAddress party={party} />
+    </Layout>
+  )
+}
+
+const ScheduleAndAddress = ({ party }) => (
+  <Box direction="row">
+    <Box fill>
+      <p>Av. Canadá 1290 / 4to Piso</p>
+      <h2>Horarios</h2>
+      <Schedule schedule={mainSchedule} />
+      <GroupsSchedule></GroupsSchedule>
+    </Box>
+    <Box>
       <Map
         latitude={party.address.latitude}
         longitude={party.address.longitude}
         text={party.name}
       />
-    </div>
-  )
-}
+    </Box>
+  </Box>
+)
+
+const GroupsSchedule = () => (
+  <div>
+    <h2>Grupos</h2>
+    <Grid
+      columns={{
+        count: 2,
+        size: "auto"
+      }}
+    >
+      {scheduleByGroup.map(x => (
+        <div key={x.name}>
+          <h4>{x.name}</h4>
+          <Schedule schedule={x.schedule} />
+        </div>
+      ))}
+    </Grid>
+  </div>
+)
+
+const Schedule = ({ schedule }) => (
+  <div>
+    {schedule.map(f => (
+      <p>{f}</p>
+    ))}
+  </div>
+)
